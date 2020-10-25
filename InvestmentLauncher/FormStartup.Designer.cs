77694ad75a08613249420935bfb4ee57e8c5474b
Jason Olchovy <jason_olchovy@hotmail.com>
2020-10-25 01:08:45 -0400

    starting on form

--- InvestmentLauncher/FormStartup.Designer.cs
+++ InvestmentLauncher/FormStartup.Designer.cs
@@ -30,7 +30,17 @@
         {
             this.buttonCancel = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxTicker = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPrincipal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxStartDate = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxEndDate = new System.Windows.Forms.TextBox();
+            this.dataGridViewPortfolio = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPortfolio)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonCancel
@@ -47,10 +57,82 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Location = new System.Drawing.Point(493, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1284, 612);
+            this.dataGridView1.Size = new System.Drawing.Size(459, 776);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // textBoxTicker
+            // 
+            this.textBoxTicker.Location = new System.Drawing.Point(78, 19);
+            this.textBoxTicker.Name = "textBoxTicker";
+            this.textBoxTicker.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTicker.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Add Ticker";
+            // 
+            // textBoxPrincipal
+            // 
+            this.textBoxPrincipal.Location = new System.Drawing.Point(78, 52);
+            this.textBoxPrincipal.Name = "textBoxPrincipal";
+            this.textBoxPrincipal.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrincipal.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Principal";
+            // 
+            // textBoxStartDate
+            // 
+            this.textBoxStartDate.Location = new System.Drawing.Point(78, 88);
+            this.textBoxStartDate.Name = "textBoxStartDate";
+            this.textBoxStartDate.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStartDate.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Start Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "End Date";
+            // 
+            // textBoxEndDate
+            // 
+            this.textBoxEndDate.Location = new System.Drawing.Point(78, 126);
+            this.textBoxEndDate.Name = "textBoxEndDate";
+            this.textBoxEndDate.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEndDate.TabIndex = 9;
+            // 
+            // dataGridViewPortfolio
+            // 
+            this.dataGridViewPortfolio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPortfolio.Location = new System.Drawing.Point(226, 22);
+            this.dataGridViewPortfolio.Name = "dataGridViewPortfolio";
+            this.dataGridViewPortfolio.Size = new System.Drawing.Size(240, 150);
+            this.dataGridViewPortfolio.TabIndex = 10;
             // 
             // FormStartup
             // 
@@ -58,12 +140,23 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonCancel;
             this.ClientSize = new System.Drawing.Size(1309, 813);
+            this.Controls.Add(this.dataGridViewPortfolio);
+            this.Controls.Add(this.textBoxEndDate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxStartDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPrincipal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTicker);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonCancel);
             this.Name = "FormStartup";
             this.Text = "InvestmentLauncher";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPortfolio)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +164,15 @@
 
         private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxTicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPrincipal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxStartDate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxEndDate;
+        private System.Windows.Forms.DataGridView dataGridViewPortfolio;
     }
 }
 
